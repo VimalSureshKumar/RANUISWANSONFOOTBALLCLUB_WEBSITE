0c8bf43184348dd7ccddf6c6e537cdde09160f76
Vimal Suresh Kumar <ac116329@avcol.school.nz>
2023-09-17 22:35:35 +1200

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace RANUISWANSONFOOTBALLCLUB_WEBSITE.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
